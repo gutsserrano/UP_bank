32588d3754c54faf7d8e90c59255df9dd0e0aa4b
veronezzi159 <lucasveronezzi3@gmail.com>
2024-06-25 09:56:21 -0300

    feat: check balance

---
+++ UP_bank/Models/DTO/AccountBalanceDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTO
+{
+    public class AccountBalanceDTO
+    {
+        public string Number { get; set; }
+        public double Balance { get; set; }
+        public double Overdraft { get; set; }
+        public double Total { get; set; }
+
+        public AccountBalanceDTO()
+        {
+            
+        }
+
+        public AccountBalanceDTO(Account account)
+        {
+            this.Number = account.Number;
+            this.Balance = account.Balance;
+            this.Overdraft = account.Overdraft;
+            this.Total = account.Balance + account.Overdraft;
+        }
+    }
+}
